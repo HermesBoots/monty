52c4490d3a667dcc7f74650e88daeb01c53d8991
HermesBoots <samuel.of.hermes@gmail.com>
2019-05-14 19:31:44 +0000

    Add placeholder definitions for operations

---
+++ placeholders.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+
+void op_add(stack_t **stack, unsigned int line_number){}
+void op_div(stack_t **stack, unsigned int line_number){}
+void op_mod(stack_t **stack, unsigned int line_number){}
+void op_mul(stack_t **stack, unsigned int line_number){}
+void op_nop(stack_t **stack, unsigned int line_number){}
+void op_pall(stack_t **stack, unsigned int line_number){}
+void op_pchar(stack_t **stack, unsigned int line_number){}
+void op_pint(stack_t **stack, unsigned int line_number){}
+void op_pop(stack_t **stack, unsigned int line_number){}
+void op_pstr(stack_t **stack, unsigned int line_number){}
+void op_push(stack_t **stack, unsigned int line_number){}
+void op_queue(stack_t **stack, unsigned int line_number){}
+void op_rotl(stack_t **stack, unsigned int line_number){}
+void op_rotr(stack_t **stack, unsigned int line_number){}
+void op_stack(stack_t **stack, unsigned int line_number){}
+void op_sub(stack_t **stack, unsigned int line_number){}
+void op_swap(stack_t **stack, unsigned int line_number){}
