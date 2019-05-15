af55290cb4691863e5c1c1fd21838903bea0f9b9
HermesBoots <samuel.of.hermes@gmail.com>
2019-05-15 16:19:03 +0000

    Add mandatory Monty operations

---
+++ stackchange.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "monty.h"
+
+
+/**
+ * op_push - push a value onto the stack
+ * @stack: pointer to value to push
+ * @line: unused
+ */
+void op_push(stack_t **stack, unsigned int line __attribute__((unused)))
+{
+	list_push(*(int *)stack);
+}
+
+
+/**
+ * op_pop - remove and print the top value of the stack
+ * @stack: unused
+ * @line: current line number in script file
+ */
+void op_pop(stack_t **stack __attribute__((unused)), unsigned int line)
+{
+	int val;
+
+	if (main_list.size < 1)
+		fail("can't pop on an empty stack", line);
+	val = list_pop();
+	printf("%d\n", val);
+}
+
+
+/**
+ * op_swap - swap the top two values in the stack
+ * @stack: unused
+ * @line: current line number in script file
+ */
+void op_swap(stack_t **stack __attribute__((unused)), unsigned int line)
+{
+	int a, b;
+
+	if (main_list.size < 2)
+		fail("can't swap, stack too short", line);
+	a = list_pop();
+	b = list_pop();
+	list_push(a);
+	list_push(b);
+}
---
+++ stackprintnum.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "monty.h"
+
+
+/**
+ * op_pall - print all numbers in the stack
+ * @stack: unused
+ * @line: current line number in script file
+ */
+void op_pall(stack_t **stack __attribute__((unused)), unsigned int line)
+{
+	long result;
+
+	if (main_list.size > 0)
+	{
+		result = list_front();
+		while (result != LIST_DONE)
+		{
+			printf("%ld\n", val);
+			result = list_next();
+		}
+	}
+}
+
+
+/**
+ * op_pint - print the number on top of the stack
+ * @stack: unused
+ * @line: current line number in script file
+ */
+void op_pint(stack_t **stack __attribute__((unused)), unsigned int line)
+{
+	if (main_list.size < 1)
+		fail("can't pint, stack empty", line);
+	printf("%d\n", list_front());
+}
